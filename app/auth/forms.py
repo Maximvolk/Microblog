d6f0bc813b88ad4e4ce4c6da5199be09f605249b
Maxim <max.ha-ha-ha@mail.ru>
2018-11-08 10:28:44 +0300

    Chapter 15: Blueprints and Application Factory (v0.15)

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
-from wtforms.validators import DataRequired, ValidationError, EqualTo, Email, Length
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from flask_babel import _, lazy_gettext as _l
 from app.models import User
-from flask_babel import lazy_gettext as _l
 
 
 class LoginForm(FlaskForm):
@@ -23,34 +23,12 @@ class RegistrationForm(FlaskForm):
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user is not None:
-            raise ValidationError(_l('Use different username.'))
+            raise ValidationError(_('Use different username.'))
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
-            raise ValidationError(_l('Use different email address.'))
-
-
-class EditProfileForm(FlaskForm):
-    username = StringField(_l('Username'), validators=[DataRequired()])
-    about_me = TextAreaField(_l('About me'), validators=[Length(min=0, max=140)])
-    submit = SubmitField(_l('Submit'))
-
-    def __init__(self, original_username, *args, **kwargs):
-        super(EditProfileForm, self).__init__(*args, **kwargs)
-        self.original_username = original_username
-
-    def validate_username(self, username):
-        if username.data != self.original_username:
-            user = User.query.filter_by(username=username.data).first()
-            if user is not None:
-                raise ValidationError(_l('Use different username.'))
-
-
-class PostForm(FlaskForm):
-    post = TextAreaField(_l('Say something'), validators=[
-        DataRequired(), Length(min=1, max=140)])
-    submit = SubmitField(_l('Submit'))
+            raise ValidationError(_('Use different email address.'))
 
 
 class ResetPasswordRequestForm(FlaskForm):
